fd8b3ef335b85d84ebef93cb3c804fdca512a329
Jerome Checketts <jeromechecketts@gmail.com>
2023-05-12 21:07:24 -0500

    Mongoose Population

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -13,6 +13,7 @@ campsiteRouter
 	})
 	.get((req, res, next) => {
 		Campsite.find()
+			.populate('comments.author')
 			.then((campsites) => {
 				res.json(campsites);
 			})
@@ -47,6 +48,7 @@ campsiteRouter
 	})
 	.get((req, res, next) => {
 		Campsite.findById(req.params.campsiteId)
+			.populate('comments.author')
 			.then((campsite) => {
 				res.json(campsite);
 			})
@@ -83,6 +85,7 @@ campsiteRouter
 	.route('/:campsiteId/comments')
 	.get((req, res, next) => {
 		Campsite.findById(req.params.campsiteId)
+			.populate('comments.author')
 			.then((campsite) => {
 				if (campsite) {
 					res.statusCode = 200;
@@ -102,6 +105,7 @@ campsiteRouter
 		Campsite.findById(req.params.campsiteId)
 			.then((campsite) => {
 				if (campsite) {
+					req.body.author = req.user._id;
 					campsite.comments.push(req.body);
 					campsite
 						.save()
@@ -157,6 +161,7 @@ campsiteRouter
 	.route('/:campsiteId/comments/:commentId')
 	.get((req, res, next) => {
 		Campsite.findById(req.params.campsiteId)
+			.populate('comments.author')
 			.then((campsite) => {
 				if (campsite && campsite.comments.id(req.params.commentId)) {
 					res.statusCode = 200;
